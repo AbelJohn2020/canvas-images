a8699ee21d4cf5f888efab978ed6efcccd9f08ea
Abel AR <abel_john_14@hotmail.com>
2021-09-09 20:34:05 -0500

    Add images list in the boards

---
+++ src/components/Board/Board.vue
@@ -0,0 +1,16 @@
+<template>
+    <div
+        :class="className"
+        :@drop="boardDrop"
+        @dragenter.prevent
+        @dragover.prevent
+    >
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,15 +1,74 @@
 <template>
     <div class="container">
+        <div
+            class="first-zone"
+        >
+            <div
+                v-for="saint in getList(1)"
+                :key="saint.id"
+                class="drag"
+            >
+                <img :src="saint.url" :alt="saint.name" class="first-image">
+            </div>
+        </div>
 
+        <div
+            class="second-zone"
+        >
+            <div
+                v-for="saint in getList(2)"
+                :key="saint.id"
+                class="drag"
+            >
+                <img :src="saint.url" :alt="saint.name">
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
+import saintseiya from '../data/data.json';
 
+export default {
+    data() {
+        return {
+            data: saintseiya,
+        };
+    },
+    methods: {
+        getList: function (list) {
+            return this.data.filter(saint => saint.list === list)
+        },
+    }
 }
 </script>
 
 <style>
+        .container {
+        box-sizing: border-box;
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        padding: 0;
+    }
 
+    .first-zone,
+    .second-zone {
+        background: #343434;
+        padding: 16px;
+        max-height: 100vh;
+    }
+
+    .first-zone {
+        width: 16%;
+        overflow-y: scroll;
+    }
+
+    .second-zone {
+        width: 84%;
+        padding: 2% 26%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 </style>
