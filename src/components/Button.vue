820df836cc8927214ceed92e0f23c68a0aeab008
Abel AR <abel_john_14@hotmail.com>
2021-09-14 15:49:20 -0500

    Add download function with axios

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -3,6 +3,7 @@
         <button
             :class="className"
             v-if="myState.cutButton === true && myState.editImage === true"
+            @click="download"
         >
             <slot />
         </button>
@@ -18,6 +19,7 @@
 </template>
 
 <script>
+    import axios from 'axios';
     export default {
         props: {
             className: String,
@@ -28,9 +30,31 @@
                 return this.$store.state;
             }
         },
+
         methods: {
-            download: () => {
-                
+            fileDownload: ( response, item ) => {
+                const headers = response.headers;
+                const dataType = response.data.type.split('/');
+                const extension = dataType[0] === 'image' ? dataType[1] : 'png';
+                const blob = new Blob([response.data], {type: headers['content-type']});
+                const link = document.createElement('a');
+                link.href = window.URL.createObjectURL(blob);
+                link.download = `${item.alt}.${extension}`;
+                link.click();
+                link.remove();
+            },
+            download() {
+                axios({
+                    method: 'get',
+                    url: this.myState.destination,
+                    responseType: 'blob',
+                })
+                    .then(res => {
+                        this.fileDownload(res, this.myState.image);
+                        this.myState.editImage = false;
+                        this.myState.cutButton = false;
+                    })
+                    .catch( e => console.log(e))
             }
         }
     }
